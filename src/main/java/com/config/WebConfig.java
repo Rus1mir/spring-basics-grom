8d6ed244b7991486563ed0f2b0df7ec3a7c8e963
Rus1mir <rusmir@ukr.net>
2019-09-05 03:08:03 +0300

    without xml

---
+++ src/main/java/com/config/WebConfig.java
@@ -0,0 +1,65 @@
+package com.config;
+
+import com.lesson1.Test;
+import com.lesson2hw1.Controller;
+import com.lesson2hw1.Route;
+import com.lesson2hw1.Step;
+import com.lesson2hw1.Service;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com")
+public class WebConfig {
+
+    @Bean()
+    DispatcherServlet dispatcherServlet() {
+        DispatcherServlet dispatcherServlet;
+        dispatcherServlet = new DispatcherServlet();
+        return dispatcherServlet;
+    }
+
+    @Bean()
+    Test test() {
+        return new Test();
+    }
+
+    @Bean()
+    Controller controller() {
+        return new Controller();
+    }
+
+    @Bean(initMethod = "beanInit")
+    @Scope("prototype")
+    Route route() {
+        Route route = new Route();
+        ArrayList<Step> steps = new ArrayList<>();
+        steps.add(step());
+        route.setSteps(steps);
+        return route;
+    }
+
+    @Bean(initMethod = "beanInit")
+    @Scope("prototype")
+    Service service() {
+        Service service = new Service();
+        service.setName("Cool");
+        return service;
+    }
+
+    @Bean(initMethod = "beanInit")
+    @Scope("prototype")
+    Step step() {
+        Step step = new Step();
+        step.setServiceFrom(service());
+        step.setServiceTo(service());
+        return step;
+    }
+}
